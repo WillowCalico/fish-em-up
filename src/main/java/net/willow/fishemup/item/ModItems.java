daaf2809e763fb5b0760c07d8ac77548735241e7
Willow <onunesmaria2007@gmail.com>
2026-04-22 01:17:59 +0100

    meow

--- src/main/java/net/willow/fishemup/item/ModItems.java
+++ src/main/java/net/willow/fishemup/item/ModItems.java
@@ -11,16 +11,29 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(FishEmUp.MOD_ID);
 
     public static final DeferredItem<FishingRodItem> netheriterod = ITEMS.register("netheriterod",
-            ()-> new FishingRodItem(new Item.Properties().stacksTo(1)));
+            ()-> new FishingRodItem(new Item.Properties()
+                    .stacksTo(1)
+                    .durability(256)
+                    .fireResistant()
+            ));
 
     public static final DeferredItem<FishingRodItem> diamondrod = ITEMS.register("diamondrod",
-            ()-> new FishingRodItem(new Item.Properties().stacksTo(1)));
+            ()-> new FishingRodItem(new Item.Properties()
+                    .stacksTo(1)
+                    .durability(128)
+            ));
 
     public static final DeferredItem<FishingRodItem> ironrod = ITEMS.register("ironrod",
-            ()-> new FishingRodItem(new Item.Properties().stacksTo(1)));
+            ()-> new FishingRodItem(new Item.Properties()
+                    .stacksTo(1)
+                    .durability(86)
+            ));
 
     public static final DeferredItem<FishingRodItem> goldrod = ITEMS.register("goldrod",
-            ()-> new FishingRodItem(new Item.Properties().stacksTo(1)));
+            ()-> new FishingRodItem(new Item.Properties()
+                    .stacksTo(1)
+                    .durability(76)
+            ));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
